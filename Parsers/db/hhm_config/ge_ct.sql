9ea25b88031f0cdab48f3594203a200491790654
Matt Teixeira <matt-teixeira@vm-debian.c.oceanic-citadel-185620.internal>
2022-12-13 20:46:52 +0000

    Track configs

---
+++ Parsers/db/hhm_config/ge_ct.sql
@@ -0,0 +1,19 @@
+UPDATE systems
+SET hhm_config = '{"file_path": "/opt/hhm-files/C0051/SHIP099/SME12446", "file_types": [{"file": "gesys", "file_suffix": "_GE-OPTIMA64.log", "datetimeVersion": "type_2"}], "modality": "CT"}'
+WHERE id = 'SME12446';
+
+UPDATE systems
+SET hhm_config = '{"file_path": "/opt/hhm-files/C0137/SHIP005/SME01096", "file_types": [{"file": "gesys", "file_suffix": "_PARMR002.log", "datetimeVersion": "type_2"}, {"file": "gesys", "file_suffix": "_mroc.log", "datetimeVersion": "type_2"}], "modality": "CT"}'
+WHERE id = 'SME01096';
+
+UPDATE systems
+SET hhm_config = '{"file_path": "/opt/hhm-files/C0137/SHIP016/SME01091", "file_types": [{"file": "gesys", "file_suffix": "_RDMCCT_2.log", "datetimeVersion": "type_2"}, {"file": "gesys", "file_suffix": "_RE36A1600120YC.log", "datetimeVersion": "type_2"}, {"file": "gesys", "file_suffix": "_ct99.log", "datetimeVersion": "type_2"}], "modality": "CT"}'
+WHERE id = 'SME01091';
+
+UPDATE systems
+SET hhm_config = '{"file_path": "/opt/hhm-files/C0137/SHIP020/SME00847", "file_types": [{"file": "gesys", "file_suffix": "_ct99.log", "datetimeVersion": "type_2"}, {"file": "gesys", "file_suffix": "_mcvct.log", "datetimeVersion": "type_2"}], "modality": "CT"}'
+WHERE id = 'SME00847';
+
+UPDATE systems
+SET hhm_config = '{"file_path": "/opt/hhm-files/C0348/SHIP002/SME10071", "file_types": [{"file": "gesys", "file_suffix": "_SCEC_CT.log", "datetimeVersion": "type_2"}, {"file": "gesys", "file_suffix": "_SCSA_CT.log", "datetimeVersion": "type_2"}, {"file": "gesys", "file_suffix": "_schct01.log", "datetimeVersion": "type_2"}], "modality": "CT"}'
+WHERE id = 'SME10071';
